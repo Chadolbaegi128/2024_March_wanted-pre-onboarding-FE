926b5c023f3977bb3fcd74e975551d4c57937bd3
elice49 <elice49@elice49ui-iMac.local>
2024-02-29 21:13:39 +0900

    feat: list 컴포넌트-할일 글, 투두 리스트 State 작성

--- vite-project/src/components/List/list.tsx
+++ vite-project/src/components/List/list.tsx
@@ -1,8 +1,13 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import Add from './add';
 
 const List: FC = () => {
+    // 할일 글 State
+    const [todo, setTodo] = useState<string>('');
 
+    // 투두 리스트 State
+    const [todoList, setTodoList] = useState<string[]>([]);
+    
     return (
         <>
             <Add/>
