267ede283079864caad4af48cd26ea6f6683eed8
YeonjunJung <duswns12893@gmail.com>
2024-03-01 06:10:39 +0900

    feat: App.tsx 재설정

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,15 +1,11 @@
-import { store } from './store';
-import { Provider } from 'react-redux';
 import List from './components/List/list';
 import './App.css'
 
 function App() { 
 
   return (
-    <>
-      <Provider store={store}>
-        <List/>
-      </Provider>
+    <>      
+        <List/>      
     </>
   )
 }
