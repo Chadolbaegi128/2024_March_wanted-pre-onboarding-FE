803f709930679ed31f83979f3bbc3c42a995a5a1
YeonjunJung <duswns12893@gmail.com>
2024-03-01 01:12:51 +0900

    feat: list 컴포넌트에 리스트 추가, 삭제 기능 추가

--- vite-project/src/components/List/list.tsx
+++ vite-project/src/components/List/list.tsx
@@ -6,10 +6,32 @@ const List: FC = () => {
     const [todo, setTodo] = useState<string>('');
 
     // 투두 리스트 State
-    const [todoList, setTodoList] = useState<string[]>([]);
+    const [todoLists, setTodoLists] = useState<string[]>([]);
 
     // 인풋창에 할일 입력 시 입력 내용을 State에 설정
     const onChangeTodo = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value);
+
+    // [Add] 버튼 클릭 시
+    const onClickAdd = () => {
+        // State 변경을 정상적으로 감지하기 위해 배열을 생성
+        const newTodoLists = [...todoLists];
+        // 인풋 창에 입력한 내용을 투두리스트 배열에 추가
+        newTodoLists.push(todo);
+        setTodoLists(newTodoLists); // => 특정 투두 항목이 추가된 투두리스트 배열로 업데이트
+
+        // 인풋 창 비우기
+        setTodo(''); 
+    };
+
+    // [Delete] 버튼 클릭 시(몇 번째 버튼이 클릭되었는지 인수로 전달)
+    const onClickDelete = (index: number) => {
+        // State 변경을 정상적으로 감지하기 위해 배열을 생성
+        const newTodoLists = [...todoLists];
+        // 투두리스트 배열로부터 해당 요소 삭제
+        newTodoLists.splice(index, 1);
+        setTodoLists(newTodoLists); // => 특정 투두 항목이 제거된 투두리스트 배열를 저장
+        
+    }
 
     return (
         <>
