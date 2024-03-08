8053879554a4af495c3532c58758cd154690eb06
YeonjunJung <duswns12893@gmail.com>
2024-03-09 07:03:08 +0900

    feat: todoSlice(할 일 항목 slice 저장소) 생성

---
+++ vite-project/src/store/todoSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+type Todo = {
+    id: number;
+    text: string;
+};
+
+type TodoState = {
+    todos: Todo[];
+};
+
+const initialState: TodoState = {
+    todos: [],
+};
+
+const todoSlice = createSlice({
+    name: 'todos',
+    initialState,
+    reducers: {
+        addTodo: (state, action: PayloadAction<Todo>) => {
+            state.todos.push(action.payload);
+        },
+        deleteTodo: (state, action: PayloadAction<number>) => {
+            state.todos.splice(action.payload, 1);
+        }
+    }
+});
+
+export const { addTodo, deleteTodo } = todoSlice.actions;
+export default todoSlice.reducer;
